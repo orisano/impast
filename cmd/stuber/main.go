d66f748ae4fbbbd156545e0024706a06eff7cd2f
Nao YONASHIRO <owan.orisano@gmail.com>
2018-01-28 04:08:53 +0900

    feat: add stuber command

---
+++ cmd/stuber/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"go/ast"
+	"go/parser"
+	"go/printer"
+	"go/token"
+	"log"
+	"os"
+
+	"github.com/orisano/pkgast"
+)
+
+func main() {
+	pkgPath := flag.String("pkg", "", "package path")
+	interfaceName := flag.String("implement", "", "implement interface name")
+	typeName := flag.String("type", "", "type name")
+	receiverName := flag.String("name", "", "receiver name")
+	flag.Parse()
+
+	pkg, err := pkgast.ImportPackage(*pkgPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	it := pkgast.FindInterface(pkg, *interfaceName)
+	if it == nil {
+		log.Fatalf("interface not found %q", *interfaceName)
+	}
+
+	body, err := parser.ParseExpr(`panic("implement me")`)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, method := range it.Methods.List {
+		decl := &ast.FuncDecl{
+			Name: method.Names[0],
+			Recv: &ast.FieldList{List: []*ast.Field{
+				{
+					Names: []*ast.Ident{ast.NewIdent(*receiverName)},
+					Type:  ast.NewIdent(*typeName),
+				},
+			}},
+			Type: method.Type.(*ast.FuncType),
+			Body: &ast.BlockStmt{List: []ast.Stmt{
+				&ast.ExprStmt{X: body},
+			}},
+		}
+		printer.Fprint(os.Stdout, token.NewFileSet(), decl)
+		os.Stdout.WriteString("\n\n")
+	}
+}
